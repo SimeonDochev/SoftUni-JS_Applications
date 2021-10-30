bb6bb1e73980641d7e7397c1b0d3e451ccaf99b6
Simeon Dochev <simeon.dochev16@gmail.com>
2021-10-30 19:27:21 +0300

    async lab

--- Asynchronous_Programming-Lab/03.Gighub-Commits/app.js
+++ Asynchronous_Programming-Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,32 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const list = document.getElementById('commits');
+
+    const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+        .then(response => {
+            if (response.ok == false) {
+                throw new Error(`${response.status}`);
+            }
+            return response.json();
+        })
+        .then(handleResponse)
+        .catch(handleError);
+    
+    function handleResponse(data) {
+        list.innerHTML = '';
+
+        for (let commit of data) {
+            const liElement = document.createElement('li');
+            liElement.textContent = `${commit.commit.author.name}: ${commit.commit.message}`;
+            list.appendChild(liElement);
+        }
+    }
+
+    function handleError(error) {
+        list.innerHTML = '';
+        list.textContent = `Error: ${error.message} (Not Found)`;
+    }
 }
