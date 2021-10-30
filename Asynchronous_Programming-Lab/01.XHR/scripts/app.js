1e2a80fb8df24efc272e1062f9f6ac7b90f3fc50
Simeon Dochev <simeon.dochev16@gmail.com>
2021-10-30 18:21:37 +0300

    async lab

--- Asynchronous_Programming-Lab/01.XHR/scripts/app.js
+++ Asynchronous_Programming-Lab/01.XHR/scripts/app.js
@@ -1,3 +1,14 @@
 function loadRepos() {
-   console.log("TODO...");
+   const resultDiv = document.getElementById('res');
+   const url = 'https://api.github.com/users/testnakov/repos';
+   const xhr = new XMLHttpRequest();
+
+   xhr.addEventListener('readystatechange', function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+         resultDiv.textContent = xhr.responseText;
+      }
+   });
+
+   xhr.open('GET', url);
+   xhr.send();
 }
