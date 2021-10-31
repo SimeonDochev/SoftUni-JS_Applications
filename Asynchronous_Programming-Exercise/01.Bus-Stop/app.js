109f47231424f3e437ecf67110fa98fa58162551
Simeon Dochev <simeon.dochev16@gmail.com>
2021-10-31 17:03:54 +0200

    async exercise

--- Asynchronous_Programming-Exercise/01.Bus-Stop/app.js
+++ Asynchronous_Programming-Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,29 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopId = document.getElementById('stopId').value;
+    const stopNameField = document.getElementById('stopName');
+    const busList = document.getElementById('buses');
+    const checkBtn = document.getElementById('submit');
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    try {
+        stopNameField.textContent = 'Loading...';
+        busList.replaceChildren();
+        checkBtn.disabled = true;
+
+        const res = await fetch(url);
+        if (res.ok == false || res.status != 200) {
+            throw new Error('Stop ID Not Found');
+        }
+        const data = await res.json();
+        stopNameField.textContent = data.name;
+        Object.entries(data.buses).forEach(bus => {
+            let newLi = document.createElement('li');
+            newLi.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`;
+            busList.appendChild(newLi);
+        })
+    } catch(error) {
+        stopNameField.textContent = 'Error';
+        checkBtn.disabled = false;
+    }
+    checkBtn.disabled = false;
 }
--- Asynchronous_Programming-Exercise/02.Bus-Schedule/app.js
+++ Asynchronous_Programming-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const infoDiv = document.getElementById('info');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let currentStopId = 'depot';
+    let stopName = '';
+
+    async function depart() {
+        departBtn.disabled = true;
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${currentStopId}`;
+        
+        const res = await fetch(url);
+        const data = await res.json();
+        
+        infoDiv.textContent = `Next Stop ${data.name}`;
+        currentStopId = data.next;
+        stopName = data.name;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+
+        infoDiv.textContent = `Arriving at ${stopName}`;
     }
 
     return {
